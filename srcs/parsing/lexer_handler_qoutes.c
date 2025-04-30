6a482d5cebce7080e2213ab52280593aa43f30d1
Youssef Bounite <ybounite@c4r10p10.1337.ma>
2025-04-30 09:28:39 +0100

    fix if have quotes and $

--- srcs/parsing/lexer_handler_qoutes.c
+++ srcs/parsing/lexer_handler_qoutes.c
@@ -6,7 +6,7 @@
 /*   By: ybounite <ybounite@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/27 10:49:42 by ybounite          #+#    #+#             */
-/*   Updated: 2025/04/27 16:28:52 by ybounite         ###   ########.fr       */
+/*   Updated: 2025/04/30 08:32:00 by ybounite         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,9 +40,7 @@ int	lenofwords_qoutes(char *str, int start)
 	}
 	return (len);
 }
-
-void	handler_qoutes(t_env_lst **list, char *str, int *i,
-	en_status state)
+void handler_qoutes(t_env_lst **list, char *str, int *i, en_status state)
 {
 	char	*ptr;
 	char	quotes;
@@ -54,18 +52,18 @@ void	handler_qoutes(t_env_lst **list, char *str, int *i,
 	{
 		ft_add_newtoken(list, ft_strdup("\"\""), state);
 		(*i) += 2;
-		return ;
+		return;
 	}
-	if (str[*i] == DOUBLE_QUOTE && str[*i + 1] == DOLLAR)
+	// Handle quoted string with $ inside
+	if (str[*i] == DOUBLE_QUOTE && ft_strchr(str + *i, '$'))
 	{
-		index = *i;
-		heandler_dollar(list, str, &index, DOLLAR);
-		*i = index;
+		process_quoted_string_with_expansion(list, str, i, state);
 		return ;
 	}
-	ptr = malloc(len + 1 * sizeof(char));
+	// Original code for handling other cases
+	ptr = malloc((len + 1) * sizeof(char));
 	if (!ptr)
-		return ;
+		return;
 	while (str[*i] && index < len)
 	{
 		if (isquotes(str[*i]))
@@ -83,4 +81,80 @@ void	handler_qoutes(t_env_lst **list, char *str, int *i,
 	}
 	ptr[index] = '\0';
 	ft_add_newtoken(list, ptr, state);
+}
+
+// New function to handle quoted strings with variable expansion
+void process_quoted_string_with_expansion(t_env_lst **list, char *str, int *i, en_status state)
+{
+	char	quotes = str[(*i)++]; // Store quote type and advance past it
+	char	*result = ft_strdup(""); // Start with empty string
+	
+	while (str[*i] && str[*i] != quotes)
+	{
+		if (str[*i] == '$')
+		{
+			(*i)++; // Skip the $			
+			// Get the variable name
+			char	*var_name = NULL;
+			int	var_len = lendollar(str, *i);
+			if (var_len > 0)
+			{
+				var_name = malloc((var_len + 1) * sizeof(char));
+				if (!var_name)
+				{
+					free(result);
+					return;
+				}
+				// Copy variable name
+				int j = 0;
+				while (j < var_len)
+					var_name[j++] = str[(*i)++];
+				var_name[j] = '\0';
+				// Get variable value
+				char	*var_value = handler_expasion(var_name);
+				// Append value to result
+				char	*temp = result;
+				result = ft_strjoin(result, var_value);
+				free(temp);
+				free(var_value);
+				free(var_name);
+			}
+			else if (str[*i - 1] == '$')
+			{
+				// Handle lone $ character
+				char *temp = result;
+				result = ft_strjoin(result, "$");
+				free(temp);
+			}
+		}
+		else
+		{
+			// Regular text - collect until $ or end of quote
+			int	text_start = *i;
+			while (str[*i] && str[*i] != quotes && str[*i] != '$')
+				(*i)++;
+			// Extract this text chunk
+			int chunk_len = *i - text_start;
+			if (chunk_len > 0)
+			{
+				char *chunk = malloc((chunk_len + 1) * sizeof(char));
+				if (!chunk)
+				{
+					free(result);
+					return;
+				}
+				// Copy chunk
+				strncpy(chunk, str + text_start, chunk_len);
+				chunk[chunk_len] = '\0';
+				// Append to result
+				char *temp = result;
+				result = ft_strjoin(result, chunk);
+				free(temp);
+				free(chunk);
+			}
+		}
+	}
+	if (str[*i] == quotes)
+		(*i)++; // Skip closing quote
+	ft_add_newtoken(list, result, state);
 }
