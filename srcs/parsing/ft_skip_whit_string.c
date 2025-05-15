0f68f9879ed19607a270e77eff77f5eca3d05e50
Youssef Bounite <dimayoussef42@gmail.com>
2025-05-15 15:22:11 +0100

    fix ft_skip_whit_string.c

--- srcs/parsing/ft_skip_whit_string.c
+++ srcs/parsing/ft_skip_whit_string.c
@@ -6,39 +6,11 @@
 /*   By: ybounite <ybounite@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/09 18:33:07 by ybounite          #+#    #+#             */
-/*   Updated: 2025/05/10 10:29:21 by ybounite         ###   ########.fr       */
+/*   Updated: 2025/05/15 15:18:53 by ybounite         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
-
-char	ft_skip_whitquotes(char *str, int *index)
-{
-	char	quote;
-
-	quote = str[(*index)++];
-	while (str[*index] && str[*index] != quote)
-		(*index)++;
-	if (str[*index])
-		(*index)++;
-	return ('Q');
-}
-
-char	ft_skip_whitoperator(char *str, int *index)
-{
-	(*index)++;
-	while (str[*index] && is_operator(str[*index]))
-		(*index)++;
-	return ('O');
-}
-
-char	ft_skip_whitword(char *str, int *index)
-{
-	while (str[*index] && !is_operator(str[*index])
-		&& !isquotes(str[*index]) && !find_space(str[*index]))
-		(*index)++;
-	return ('W');
-}
 
 int	skip_strqoutes(char *str, int *inedx, char quotes)
 {
@@ -77,4 +49,17 @@ void	skip_whiteword(char *str, int *i)
 		else
 			(*i)++;
 	}
+}
+
+void	ft_skip_qutes(char *str, int *start, int *len)
+{
+	char	qoutes;
+
+	qoutes = str[(*start)++];
+	while (str[*start] && str[*start] != qoutes)
+	{
+		(*start)++;
+		(*len)++;
+	}
+	(*start)++;
 }
