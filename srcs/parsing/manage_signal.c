c319190e6d374797599360a3dbf09aabc1a7144a
Youssef Bounite <dimayoussef42@gmail.com>
2025-05-15 14:23:39 +0100

    finish signal

--- srcs/parsing/manage_signal.c
+++ srcs/parsing/manage_signal.c
@@ -6,7 +6,7 @@
 /*   By: ybounite <ybounite@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/04 13:44:50 by ybounite          #+#    #+#             */
-/*   Updated: 2025/05/11 16:24:52 by ybounite         ###   ########.fr       */
+/*   Updated: 2025/05/15 14:23:02 by ybounite         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,13 +21,41 @@ void	handler(int signum)
 	rl_redisplay();
 }
 
+void	set_signals_for_heredoc(int va_signal)
+{
+	if (va_signal == SIGINT)
+	{
+		rl_free_line_state();
+		rl_cleanup_after_signal();
+		close(data_struc()->heredoc_fd);
+		ft_malloc(false, false);
+		exit(130);
+	}
+}
+
+void	handler_sigint(int signum)
+{
+	(void)signum;
+	printf("\n");
+	rl_on_new_line();
+	rl_replace_line("", 0);
+	rl_redisplay();
+	exit(130);
+}
+
+void	hendle_sigquit(int signum)
+{
+	(void)signum;
+	exit(131);
+}
+
 void	assign_signals_handler(void)
 {
-	struct termios	term;
+	// struct termios	term;
 
-	tcgetattr(STDIN_FILENO, &term);
-	// term.c_lflag &= ~ECHOCTL;
-	tcsetattr(STDIN_FILENO, TCSANOW, &term);
+	// tcgetattr(STDIN_FILENO, &term);
+	// // term.c_lflag &= ~ECHOCTL;
+	// tcsetattr(STDIN_FILENO, TCSANOW, &term);
 	signal(SIGQUIT, SIG_IGN);
 	signal(SIGINT, handler);
 }
