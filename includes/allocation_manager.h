361cc58144f26584d3242902637829ee2fab509c
Youssef Bounite <dimayoussef42@gmail.com>
2025-05-15 16:17:51 +0100

    by hell

--- includes/allocation_manager.h
+++ includes/allocation_manager.h
@@ -6,19 +6,19 @@
 /*   By: ybounite <ybounite@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/30 13:32:08 by ybounite          #+#    #+#             */
-/*   Updated: 2025/04/30 14:29:23 by ybounite         ###   ########.fr       */
+/*   Updated: 2025/05/15 16:16:35 by ybounite         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef	ALLOCATION_MANAGER_H
-#define ALLOCATION_MANAGER_H
+#ifndef ALLOCATION_MANAGER_H
+# define ALLOCATION_MANAGER_H
 
-#include "../libraries/libft/libft.h"
+# include "../libraries/libft/libft.h"
 
 /* -------------------------------------------------------------------------- */
 /*                               allocate_data.c                              */
 /* -------------------------------------------------------------------------- */
-void							*ft_malloc(size_t size, short option);
-void							memory_released(t_list **head);
+void						*ft_malloc(size_t size, short option);
+void						memory_released(t_list **head);
 
 #endif
